d56c80a1227856ae77d669526eb976fe39fd28dd
Arpita Gupta <arpitagupta2578@gmail.com>
2025-12-10 23:31:58 +0530

    start the is valid logic

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -1,6 +1,12 @@
 class SudokuSolver:
-    def __init__(self):
+    def __init__(self, board):
         print("Hello")
+
 
     def solve(self):
         print("Hello")
+
+    def is_valid():
+        current_num = 0
+        if current_num in row or current_num in col :
+            print("Cannot add this number as it already exists in the row.")
